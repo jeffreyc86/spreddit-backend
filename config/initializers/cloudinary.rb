f372eec3f0662f71c93d085c843a91504fe6485e
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-12 13:29:25 -0500

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,8 @@
+
+Cloudinary.config do |config|    
+    config.cloud_name = "dixx0jhhd"   
+    config.api_key = "689152463972241"   
+    config.api_secret = "GAE6LChFvZFvEs0Y_h26J8qV_cA"   
+    config.secure = true    
+    config.cdn_subdomain = true  
+ end
