dbef21caea00e6c5381ff3f4b32875a927017875
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-17 09:11:52 -0500

    login signup page complete

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|    
-    config.cloud_name = "dixx0jhhd"   
-    config.api_key = "689152463972241"   
-    config.api_secret = "GAE6LChFvZFvEs0Y_h26J8qV_cA"   
+    config.cloud_name = "dfpkfgg80"   
+    config.api_key = "731271193416168"   
+    config.api_secret = "19W2UrcB3-78Nom-gQFS5Ej6L0w"   
     config.secure = true    
     config.cdn_subdomain = true  
  end
